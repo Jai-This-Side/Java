69265895ba16279121e8086e379508e62b708fd2
Jai??? <130891128+Jai-This-Side@users.noreply.github.com>
2024-12-22 10:59:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-public class Hello{
-    public static void main(string args[]) {
+public class Main{
+    public static void main(String args[]) {
         System.out.println("Hello World");
     }
 }
